b66061a13c5093c9372294dbc2027e4ad4e970a2
Antoni Débaz <pi46vew@eduvaud.ch>
2026-04-30 16:19:32 +0200

    Changement du thème du jeu et mise à jour des sprites

--- CoffeverGame/script.js
+++ CoffeverGame/script.js
@@ -1,5 +1,5 @@
 var joueur = document.querySelector("#joueur");
-var voleur = document.querySelector("#voleur");
+var obstacle = document.querySelector("#obstacle");
 var score = document.querySelector("#score");
 var cartoonBean = document.querySelector("#cartoonBean");
 var ecranDeMort = document.querySelector("#ecranDeMort");
@@ -9,78 +9,97 @@ var mort = false;
 ecranDeMort.style.display = "none";
 cartoonBean.style.visibility = "hidden";
 
+var timeOfOneFall = 1000;
 
-var positionXJoueur = 10;
-var positionXVoleur = 10;
+var positionXJoueur = 60;
+var positionXObstacle = 0;
+var positionYObstacle = 0;
+
 var scoreInt = 0;
-var multiplyer = 1;
 
 //Calculateur de points
 setInterval(() => {
     if(mort === false)
     {
-        multiplyer = multiplyer + 0.01;
-        scoreInt = scoreInt + (1*multiplyer);
+        scoreInt = scoreInt + 1000/timeOfOneFall;
         scoreInt = Math.round(scoreInt)
         score.textContent = "Score : " + scoreInt;
     }
 }, 100)
 
-//Actualisation de la positionX du joueur
-setInterval(() => {
-    joueur.style.transform = "translate(calc("+positionXJoueur+"vw + -5vh),60vh)";
-}, 10)
 
-//Actualisation de la positionX du voleur
-setInterval(() => {
-    voleur.style.transform = "translateX(calc("+positionXVoleur+"vw + -5vh))";
+obstacle.style.transition = "transform 0ms linear";
+obstacle.style.transform = "translateX("+positionXObstacle+"vw)";
 
-    var random = Math.random()
-
-    if(random > 0.5 && positionXVoleur > 5)
+//Actualisation de la positionX de l'obstacle quand elle apparait en haut aléatoirement puis de sa chute
+setInterval(()=>{
+    if(mort === false)
     {
-        positionXVoleur = positionXVoleur - 5;
+        if(positionYObstacle < 60)
+        {
+            positionYObstacle = positionYObstacle + 600/timeOfOneFall;
+        }
+        else
+        {
+            var randomPos = Math.random()
+            positionYObstacle = 0;
+            if(randomPos > 0.666)
+            {
+                positionXObstacle = 0;
+            }
+            else if(randomPos < 0.666 && randomPos > 0.333)
+            {
+                positionXObstacle = 30;
+            }
+            else{
+                positionXObstacle = 60;
+            }
+
+            
+            var randomCar = Math.random()
+            if(randomCar > 0.666)
+            {
+                obstacle.src="../img/voiture1.png";
+            }
+            else if(randomCar < 0.666 && randomPos > 0.333)
+            {
+                obstacle.src="../img/voiture2.png";
+            }
+            else{
+                obstacle.src="../img/voiture3.png";
+            }
+
+            timeOfOneFall = timeOfOneFall-(timeOfOneFall/100);
+
+            console.log(timeOfOneFall);
+        }
     }
-        if(random < 0.5 && positionXVoleur < 85)
-    {
-        positionXVoleur = positionXVoleur + 5;
-    }
-}, 300)
+},10)
 
 //detection des touches
 document.addEventListener('keydown', function(event) {
-    console.log(`Key pressed: ${event.key}`)
     
-    if(event.key === 'a'&& positionXJoueur > 5)
+    if(event.key === 'a'&& positionXJoueur > 0)
     {
-        positionXJoueur = positionXJoueur - 5;
+        positionXJoueur = positionXJoueur - 30;
     }
     
-    if(event.key === 'd' && positionXJoueur < 85)
+    if(event.key === 'd' && positionXJoueur < 60)
     {
-        positionXJoueur = positionXJoueur + 5;
+        positionXJoueur = positionXJoueur + 30;
     }
 })
 
-
-
-//fait bouger le grain de café
-setInterval(()=>{
-    cartoonBean.style.transition = "transform 1000ms linear";
-    cartoonBean.style.transform = "translate(calc("+positionXJoueur+"vw + -5vh),75vh)";
-    cartoonBean.style.visibility = "visible";
-    setTimeout(function(){
-        cartoonBean.style.visibility = "hidden";
-        cartoonBean.style.transition = "transform 0ms linear";
-        cartoonBean.style.transform = "translate(calc("+positionXVoleur+"vw + -5vh),0vh)";
-    }, 1000);
-    
-},2000)
+//Actualisation de la positionX du joueur et la position de l'obstacle
+setInterval(() => {
+    joueur.style.transform = "translate("+positionXJoueur+"vw,60vh)";
+    obstacle.style.transform = "translate("+positionXObstacle+"vw,"+positionYObstacle+"vh)";
+}, 10)
 
 //getBoundingClientRect sert a récupérer la taille et la position
 //cette fonction sert à detecter les collisions entre le grains de café et la voiture
 setInterval(()=>{
-    if(cartoonBean.getBoundingClientRect().bottom > joueur.getBoundingClientRect().top && cartoonBean.getBoundingClientRect().top < joueur.getBoundingClientRect().bottom && cartoonBean.getBoundingClientRect().right > joueur.getBoundingClientRect().left && cartoonBean.getBoundingClientRect().left < joueur.getBoundingClientRect().right)
+    if(obstacle.getBoundingClientRect().bottom > joueur.getBoundingClientRect().top && obstacle.getBoundingClientRect().top < joueur.getBoundingClientRect().bottom && obstacle.getBoundingClientRect().right > joueur.getBoundingClientRect().left && obstacle.getBoundingClientRect().left < joueur.getBoundingClientRect().right)
     {
         ecranDeMort.style.display = "flex";
         ecranDeJeu.style.display = "none";
